13559dcf155c367fdad1dcd94bd0154fc057adfb
premnath018 <prem185nath@gmail.com>
2023-11-04 15:32:10 +0530

    Login & SignUp Completed

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -6,7 +6,7 @@
 <head>
 
         <meta charset="utf-8" />
-        <title>Sign Up | Hybrix - Admin & Dashboard Template</title>
+        <title>Sign Up | School Portal</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Minimal Admin & Dashboard Template" name="description" />
         <meta content="Themesbrand" name="author" />
